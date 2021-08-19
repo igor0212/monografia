1db2a5532c06f6d1239321b9980073fd54a4b824
Igor Oliveira <igors@ciandt.com>
2021-08-19 00:23:57 -0300

    clean job

--- scripts/inserts_job.sql
+++
@@ -1,9 +0,0 @@
--------------------------------------------------------- Script shot in 2021-08-18 ------------------------------------------------------------------------------------------
-
-INSERT INTO public."Job"(
-name, created_on, has_error)
-VALUES ('get_apartment', '2021-08-18 20:02:00.53098', False); 
-
-INSERT INTO public."Job"(
-name, created_on, has_error)
-VALUES ('get_house', '2021-08-18 23:20:18.014045', True); 
