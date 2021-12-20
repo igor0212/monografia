a17168d4b13a9a85f615dd77826b3829053dc42b
Igor Silveira <Igor.Silveira@us.panasonic.com>
2021-12-20 11:43:47 -0300

    2021-12-20

---
+++ log/2021-12-20.sql
@@ -0,0 +1,21 @@
+
+
+ -------------------------------------------------------- Script shot in 2021-12-20 04:03:22.012071 ------------------------------------------------------------------------------------------ 
+
+ no new properties found 
+
+
+
+ -------------------------------------------------------- Script shot in 2021-12-20 06:20:38.169028 ------------------------------------------------------------------------------------------ 
+
+ INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6075900', 225000, 0, 0, '2021-12-20 04:47:25.624820', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6482145', 360000, 147, 270, '2021-12-20 05:40:21.992265', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6711125', 259000, 0, 0, '2021-12-20 05:56:02.139989', False); 
+ 
+
+
+
+ -------------------------------------------------------- Script shot in 2021-12-20 06:20:38.379405 ------------------------------------------------------------------------------------------ 
+
+ INSERT INTO public."Job"(name, created_on, has_error, error_description) VALUES ('routine', '2021-12-20 06:20:38.184615', False, ''); 
+
