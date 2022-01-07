7145af0245c681d14648e05c4abc28f3fac5c15d
Igor Silveira <Igor.Silveira@us.panasonic.com>
2022-01-07 09:42:04 -0300

    2022-01-07

---
+++ log/2022-01-07.sql
@@ -0,0 +1,53 @@
+
+
+ -------------------------------------------------------- Script shot in 2022-01-07 04:01:07.699940 ------------------------------------------------------------------------------------------ 
+
+ no new properties found 
+
+
+
+ -------------------------------------------------------- Script shot in 2022-01-07 06:18:39.431872 ------------------------------------------------------------------------------------------ 
+
+ INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('5759857', 850000, 0, 0, '2022-01-07 04:06:11.111999', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('5885637', 559000, 0, 1, '2022-01-07 04:16:36.969315', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('5931973', 560000, 159, 300, '2022-01-07 04:23:27.616374', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('5959831', 1140000, 198, 730, '2022-01-07 04:24:31.386859', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('5959999', 320000, 0, 0, '2022-01-07 04:25:49.213458', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6015567', 1301226, 0, 0, '2022-01-07 04:31:33.988909', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6015581', 605139, 0, 0, '2022-01-07 04:31:41.858738', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6059262', 230000, 60, 260, '2022-01-07 04:32:49.759806', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6094811', 230000, 400, 250, '2022-01-07 04:46:38.070794', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6089450', 1200000, 250, 700, '2022-01-07 04:54:45.366554', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6097497', 475000, 0, 0, '2022-01-07 04:58:15.450734', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6164338', 450000, 198, 0, '2022-01-07 05:04:48.107749', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6040042', 230000, 60, 260, '2022-01-07 05:13:00.390253', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6247373', 560000, 0, 0, '2022-01-07 05:19:07.250706', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6372740', 700000, 195, 896, '2022-01-07 05:28:07.376287', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6372742', 830000, 350, 1100, '2022-01-07 05:29:00.840977', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6390227', 1650000, 907, 1680, '2022-01-07 05:29:24.233651', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6390246', 450000, 0, 0, '2022-01-07 05:30:27.885634', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6418613', 350000, 0, 0, '2022-01-07 05:30:58.694120', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6515122', 890000, 500, 0, '2022-01-07 05:41:12.189383', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6554368', 309000, 92, 250, '2022-01-07 05:43:53.597272', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6634998', 245000, 66, 240, '2022-01-07 05:48:14.785668', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6747607', 180000, 670, 240, '2022-01-07 05:55:55.314513', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6754171', 559000, 0, 1, '2022-01-07 05:58:16.689354', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6801031', 238990, 67, 290, '2022-01-07 06:03:03.088231', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6784381', 238990, 67, 290, '2022-01-07 06:03:04.074328', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6801504', 185000, 0, 230, '2022-01-07 06:03:17.206803', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6841682', 165000, 70, 228, '2022-01-07 06:05:57.862801', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6888916', 360000, 160, 330, '2022-01-07 06:12:00.164392', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6888863', 405000, 110, 0, '2022-01-07 06:12:24.115022', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6918771', 325000, 129, 200, '2022-01-07 06:13:52.585980', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('2447051', 359000, 0, 0, '2022-01-07 06:14:22.706013', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6452273', 352000, 0, 0, '2022-01-07 06:14:42.932904', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('2207484', 390000, 55, 180, '2022-01-07 06:16:07.499359', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6936878', 600000, 0, 500, '2022-01-07 06:16:29.970008', False); 
+ 
+
+
+
+ -------------------------------------------------------- Script shot in 2022-01-07 06:18:39.633763 ------------------------------------------------------------------------------------------ 
+
+ INSERT INTO public."Job"(name, created_on, has_error, error_description) VALUES ('routine', '2022-01-07 06:18:39.439871', False, ''); 
+
