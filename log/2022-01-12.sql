57bbe1991a19d44b9f9ec6a59e03e4cd335c5965
Igor Silveira <Igor.Silveira@us.panasonic.com>
2022-01-12 09:20:53 -0300

    2022-01-12

---
+++ log/2022-01-12.sql
@@ -0,0 +1,25 @@
+
+
+ -------------------------------------------------------- Script shot in 2022-01-12 03:54:55.375797 ------------------------------------------------------------------------------------------ 
+
+ no new properties found 
+
+
+
+ -------------------------------------------------------- Script shot in 2022-01-12 06:08:34.120137 ------------------------------------------------------------------------------------------ 
+
+ INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6177858', 295000, 1200, 341, '2022-01-12 04:58:37.513025', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6177857', 245000, 77, 175, '2022-01-12 04:59:20.438678', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6701194', 590000, 140, 792, '2022-01-12 05:42:27.135234', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6701193', 490000, 1003, 250, '2022-01-12 05:42:28.003661', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6701192', 380000, 100, 219, '2022-01-12 05:42:29.216948', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6787682', 170000, 60, 300, '2022-01-12 05:52:37.493375', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6918772', 330000, 100, 350, '2022-01-12 06:03:47.456714', False); 
+ 
+
+
+
+ -------------------------------------------------------- Script shot in 2022-01-12 06:08:34.326247 ------------------------------------------------------------------------------------------ 
+
+ INSERT INTO public."Job"(name, created_on, has_error, error_description) VALUES ('routine', '2022-01-12 06:08:34.135761', False, ''); 
+
