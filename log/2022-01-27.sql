3278eac2a55931ac06a04d098ccc41982938bacc
Igor Silveira <Igor.Silveira@us.panasonic.com>
2022-01-27 09:05:53 -0300

    2022-01-27

---
+++ log/2022-01-27.sql
@@ -0,0 +1,22 @@
+
+
+ -------------------------------------------------------- Script shot in 2022-01-27 04:25:57.372653 ------------------------------------------------------------------------------------------ 
+
+ no new properties found 
+
+
+
+ -------------------------------------------------------- Script shot in 2022-01-27 06:51:18.627965 ------------------------------------------------------------------------------------------ 
+
+ INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6076107', 235000, 0, 0, '2022-01-27 05:01:51.898021', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6747937', 210000, 70, 369, '2022-01-27 06:25:27.366496', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6747735', 405000, 118, 180, '2022-01-27 06:26:31.054441', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6828173', 380000, 0, 400, '2022-01-27 06:36:26.170386', False); 
+ 
+
+
+
+ -------------------------------------------------------- Script shot in 2022-01-27 06:51:18.810638 ------------------------------------------------------------------------------------------ 
+
+ INSERT INTO public."Job"(name, created_on, has_error, error_description) VALUES ('routine', '2022-01-27 06:51:18.634965', False, ''); 
+
