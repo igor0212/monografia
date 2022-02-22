0ecb2ad32640044716d2a8e16792db963c9236c4
Igor Silveira <Igor.Silveira@us.panasonic.com>
2022-02-22 09:26:44 -0300

    2022-02-22

---
+++ log/2022-02-22.sql
@@ -0,0 +1,23 @@
+
+
+ -------------------------------------------------------- Script shot in 2022-02-22 03:49:37.868688 ------------------------------------------------------------------------------------------ 
+
+ no new properties found 
+
+
+
+ -------------------------------------------------------- Script shot in 2022-02-22 04:34:47.618520 ------------------------------------------------------------------------------------------ 
+
+ INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('221615', 1050000, 0, 0, '2022-02-22 03:59:08.244715', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('5976033', 1475000, 0, 0, '2022-02-22 04:12:44.300344', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('5959853', 158000, 0, 0, '2022-02-22 04:12:59.043699', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6059212', 420000, 160, 0, '2022-02-22 04:16:03.743317', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6015594', 1750000, 700, 1960, '2022-02-22 04:17:41.422288', False); 
+ 
+
+
+
+ -------------------------------------------------------- Script shot in 2022-02-22 04:34:47.793034 ------------------------------------------------------------------------------------------ 
+
+ INSERT INTO public."Job"(name, created_on, has_error, error_description) VALUES ('routine', '2022-02-22 04:34:47.618520', False, ''); 
+
