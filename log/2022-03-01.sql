ff809ddf90abde6fb49128c2650de23a31c589de
Igor Silveira <Igor.Silveira@us.panasonic.com>
2022-03-01 13:02:41 -0300

    2022-03-01

---
+++ log/2022-03-01.sql
@@ -0,0 +1,23 @@
+
+
+ -------------------------------------------------------- Script shot in 2022-03-01 04:17:06.383378 ------------------------------------------------------------------------------------------ 
+
+ no new properties found 
+
+
+
+ -------------------------------------------------------- Script shot in 2022-03-01 06:29:44.183505 ------------------------------------------------------------------------------------------ 
+
+ INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6075941', 229000, 0, 0, '2022-03-01 04:59:17.240700', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6075890', 295000, 0, 0, '2022-03-01 04:59:19.435186', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6076057', 360000, 0, 0, '2022-03-01 05:05:34.574517', False); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6075931', 320000, 0, 0, '2022-03-01 05:25:46.512890', True); 
+INSERT INTO public."Management"(partner_id, price, tax_rate, property_tax, created_on, is_available) VALUES ('6761489', 375000, 84, 370, '2022-03-01 06:09:49.339210', True); 
+ 
+
+
+
+ -------------------------------------------------------- Script shot in 2022-03-01 06:29:44.277789 ------------------------------------------------------------------------------------------ 
+
+ INSERT INTO public."Job"(name, created_on, has_error, error_description) VALUES ('routine', '2022-03-01 06:29:44.195914', False, ''); 
+
