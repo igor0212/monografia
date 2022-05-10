27578750fadd3fa2424e96115fecb0f83f3e80ee
Igor Oliveira <42194876+igor0212@users.noreply.github.com>
2022-05-10 00:53:19 -0300

    Create district test

---
+++ src/Tests/UnitTests/Service/district_test.py
@@ -0,0 +1,9 @@
+from Service.district import District
+from Repository.district import District as RepositoryDistrict
+from unittest.mock import MagicMock
+
+class TestDistrict:
+    def test_get_all(self):
+        RepositoryDistrict.get_all = MagicMock(return_value = [{'id': 2, 'name': 'Buritis', 'region_id': 3}, {'id': 3, 'name': 'Padre Eustáquio', 'region_id': 4}, {'id': 4, 'name': 'Alto Vera Cruz', 'region_id': 2}, {'id': 5, 'name': 'Jardim dos Comerciários', 'region_id': 9}])
+        users = District.get_all()       
+        assert len(users) == 4
