14328652d473ffa8c1e7581c544e5769d53680ee
Igor Oliveira <42194876+igor0212@users.noreply.github.com>
2022-05-09 22:28:47 -0300

    Create unit test for example

---
+++ src/Tests/Service/city_test.py
@@ -0,0 +1,7 @@
+from Service.city import City
+
+class TestCity:
+    def test_upper(self):
+        #users = City.get_all()       
+        #assert len(users) == 1
+        assert 'foo'.upper() == 'FOO'
