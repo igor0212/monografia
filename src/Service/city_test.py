4e130bc5c4a3c1f806f640cf99bf586aeca71d3a
Igor Oliveira <42194876+igor0212@users.noreply.github.com>
2022-05-10 00:17:03 -0300

    Mock repository

--- src/Service/city_test.py
+++ src/Service/city_test.py
@@ -1,8 +1,9 @@
 from city import City
+from Repository.city import City as RepositoryCity
 from unittest.mock import MagicMock
 
 class TestCity:
-    def test_get_all(mocker):
-        City.get_all = MagicMock(return_value = [{'id': 1, 'name': 'Belo Horizonte'}])        
+    def test_get_all(self):
+        RepositoryCity.get_all = MagicMock(return_value = [{'id': 1, 'name': 'Belo Horizonte'}])        
         users = City.get_all()       
         assert len(users) == 1
