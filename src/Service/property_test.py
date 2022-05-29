980454a7e8919c636b866df83b59666a4c72f6d1
Pedro Souza <pedroaesouza@gmail.com>
2022-05-29 15:33:49 -0300

    Add get property by partner code tests

--- src/Service/property_test.py
+++ src/Service/property_test.py
@@ -12,7 +12,7 @@ class TestProperty:
         properties = Property.get_all()
         assert len(properties) == 3
 
-    def test_get_all_should_return_empty_list(self):
+    def test_get_all_should_return_empty_list_when_not_found(self):
         RepositoryProperty.get_all = MagicMock(return_value=[])
         properties = Property.get_all()
         assert properties == []
@@ -28,7 +28,7 @@ class TestProperty:
         properties = Property.get_all_new_ad()
         assert len(properties) == 2
 
-    def test_get_all_new_ad_should_return_empty_list(self):
+    def test_get_all_new_ad_should_return_empty_list_when_not_found(self):
         RepositoryProperty.get_all_new_ad = MagicMock(return_value=[])
         properties = Property.get_all_new_ad()
         assert properties == []
@@ -38,19 +38,19 @@ class TestProperty:
         with pytest.raises(Exception, match=r".*Partner Service - get_all_new_ad error:*"):
             Property.get_all_new_ad()
 
-    def test_get_by_partner_id_should_return_property_when_it_is_found(self):
+    def test_get_by_partner_id_should_return_property_when_found(self):
         id = 3
         RepositoryProperty.get_by_partner_id = MagicMock(id, return_value=[{'id': 1, 'city_id': 2, 'partner_id': 3}])
         property = Property.get_by_partner_id(id)
         assert id == property.get('partner_id')
 
-    def test_get_by_partner_id_should_return_empty_object_when_it_is_not_found(self):
+    def test_get_by_partner_id_should_return_empty_object_when_not_found(self):
         id = 1
         RepositoryProperty.get_by_partner_id = MagicMock(id, return_value=[])
         property = Property.get_by_partner_id(id)
         assert property == {}
 
-    def test_get_by_partner_id_should_raise_exception_if_invalid(self):
+    def test_get_by_partner_id_should_raise_exception_when_invalid_result(self):
         id = 3
         RepositoryProperty.get_by_partner_id = MagicMock(id, return_value={'any': 'invalid value'})
         with pytest.raises(Exception, match=r".*"):
@@ -61,3 +61,27 @@ class TestProperty:
         RepositoryProperty.get_by_partner_id = MagicMock(side_effect=Exception)
         with pytest.raises(Exception, match=r".*Partner Service - get_by_partner_id error:*"):
             Property.get_by_partner_id(id)
+
+    def test_get_by_partner_code_should_return_property_when_found(self):
+        code = 'xyz'
+        RepositoryProperty.get_by_partner_code = MagicMock(code, return_value=[{'id': 1, 'partner_code': 'xyz'}])
+        property = Property.get_by_partner_code(code)
+        assert code == property.get('partner_code')
+
+    def test_get_by_partner_code_should_return_empty_object_when_not_found(self):
+        code = 'xyz'
+        RepositoryProperty.get_by_partner_code = MagicMock(code, return_value=[])
+        property = Property.get_by_partner_code(code)
+        assert property == {}
+
+    def test_get_by_partner_code_should_when_invalid_result(self):
+        code = 'xyz'
+        RepositoryProperty.get_by_partner_code = MagicMock(code, return_value={'result': 'invalid'})
+        with pytest.raises(Exception, match=r".*"):
+            Property.get_by_partner_code(code)
+
+    def test_get_by_partner_code_should_raise_exception(self):
+        code = 'xyz'
+        RepositoryProperty.get_by_partner_code = MagicMock(side_effect=Exception)
+        with pytest.raises(Exception, match=r".*Partner Service - get_by_partner_code error:*"):
+            Property.get_by_partner_code(code)
