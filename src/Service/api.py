11948edd7011d4665009f8b01b7853e10c8775cc
Igor Oliveira <42194876+igor0212@users.noreply.github.com>
2022-06-14 22:45:33 -0300

    Tests

--- src/Service/api.py
+++ src/Service/api.py
@@ -1,18 +1,19 @@
 import flask
 from flask import jsonify
-from Service.management import Management
-from Service.city import City
-from Service.district import District
-from Service.property import Property
-from Service.partner import Partner
-from Service.liquidity import Liquidity
-from Service.region import Region
+from management import Management
+from city import City
+from district import District
+from property import Property
+from partner import Partner
+from liquidity import Liquidity
+from region import Region
 from util import Util, JSONEncoder
 
-class Api:
-    app = flask.Flask(__name__)
-    app.json_encoder = JSONEncoder
+app = flask.Flask(__name__)
+app.json_encoder = JSONEncoder
 
+class Api:        
+    
     @app.errorhandler(404)
     def page_not_found(e):
         return "<h1>404</h1><p>The resource could not be found.</p>", 404
