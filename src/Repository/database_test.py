970d47c25b2577d00a3964545e24eb70226754d7
Igor Oliveira <42194876+igor0212@users.noreply.github.com>
2022-06-14 23:44:47 -0300

    rename methods

--- src/Repository/database_test.py
+++ src/Repository/database_test.py
@@ -1,27 +1,27 @@
 from database import DataBase
 
 class TestDatabase:
-    def test_select_city(self):
+    def test_select_all_when_it_finds_cities(self):
         query = 'select * from "City"'            
         cities = DataBase.select(query)        
         assert len(cities) == 1
     
-    def test_select_goal(self):
+    def test_select_all_when_it_finds_goals(self):
         query = 'select * from "Goal"'            
         goals = DataBase.select(query)        
         assert len(goals) == 2
 
-    def test_select_type(self):
+    def test_select_all_when_it_finds_types(self):
         query = 'select * from "Type"'            
         types = DataBase.select(query)        
         assert len(types) == 3
 
-    def test_select_district(self):
+    def test_select_all_when_it_finds_districts(self):
         query = 'select * from "District"'            
         districts = DataBase.select(query)        
         assert len(districts) == 488
 
-    def test_select_region(self):
+    def test_select_all_when_it_finds_regions(self):
         query = 'select * from "Region"'            
         regions = DataBase.select(query)        
         assert len(regions) == 9
