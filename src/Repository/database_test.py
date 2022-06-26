9d5f03a8e63da472b94e49abe26c192a0078088c
Renan Maia <renanmaiabh@gmail.com>
2022-06-26 19:51:29 -0300

    insertion test region fix

--- src/Repository/database_test.py
+++ src/Repository/database_test.py
@@ -31,7 +31,7 @@ class TestDatabase:
     #     query = 'insert '
     
     def test_insertion_region(self):
-        query = 'INSERT INTO public."Region" ("name") VALUES("RegiãoTeste")';
+        query = 'INSERT INTO public."Region" VALUES ("RegiãoTeste");'
         regions = DataBase.insert(query)
         assert 'RegiãoTest' == DataBase.select('select name from public.Region where name = "RegiãoTest"')
     
