fa03d81530330785b9b6a4a428bc3c8665fe3f33
Renan Maia <renanmaiabh@gmail.com>
2022-06-26 22:50:12 -0300

    ajuste BD nos testes

--- src/Repository/database_test.py
+++ src/Repository/database_test.py
@@ -18,24 +18,24 @@ class TestDatabase:
         assert len(types) == 3
 
     def test_select_all_when_it_finds_districts(self):
+        DataBase.update('DELETE FROM "District" where name =' + "'BairroTeste'")
         query = 'select * from "District"'            
         districts = DataBase.select(query)        
         assert len(districts) == 488
 
     def test_select_all_when_it_finds_regions(self):
+        DataBase.update('DELETE FROM "Region" where id >= 10')
         query = 'select * from "Region"'            
         regions = DataBase.select(query)        
         assert len(regions) == 9
 
     def test_insertion_district(self):
-        DataBase.update('DELETE FROM "District" where name =' + "'BairroTeste'")
         query = 'INSERT INTO "District" ("id", "name") VALUES' + "(1010, 'BairroTeste')"
         district = DataBase.insert(query)
         existing = DataBase.select('select name from "District" where id = 1010')
         assert [{'name': 'BairroTeste'}] == existing
     
     def test_insertion_region(self):
-        DataBase.update('DELETE FROM "Region" where id >= 10')
         query = 'INSERT INTO "Region" ("id", "name") VALUES' + "(1010, 'RegiaoTeste')"
         regions = DataBase.insert(query)
         existing = DataBase.select('select name from "Region" where id = 1010')
